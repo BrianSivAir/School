29fecc6bdf3fd15c87906bcf5bc18929885ba0b3
XTRIGGER00 <sivieri.brian@gmail.com>
2022-04-07 18:44:56 +0200

    Simulazione Verifica Aprile 2022 con file .dat binario

--- 3K/C_C++/Simulazione_Verifica_Aprile_2022/1.cpp
+++ 3K/C_C++/Simulazione_Verifica_Aprile_2022/1.cpp
@@ -6,29 +6,19 @@
 using namespace std;
 
 struct voce {
-  int CodFattura;
-  int CodCliente;
-  string Data;
-  float Importo;
-  string Pagato;
-};
+  char CodFattura[255];
+  char CodCliente[255];
+  char Data[255];
+  char Importo[255];
+  char Pagato[255];
+} voce0;
 
-struct results {
-  string CodFattura[255];
-  string CodCliente[255];
-  string Data[255];
-  string Importo[255];
-  string Pagato[255];
-};
-
-struct results r;
-
-void write (struct voce data);
-int find (string token);
+void write ();
+void fsearch (char token[]);
+void out(bool match);
 
 int main() {
   while (true) {
-    struct voce data;
     int choice;
 
     cout << "Scegliere un opzione:\n" << endl;
@@ -38,36 +28,25 @@ int main() {
     cin >> choice;
     if (choice == 1) {
       cout << "Codice fattura: ";
-      cin >> data.CodFattura;
+      cin >> voce0.CodFattura;
       cout << "Codice cliente: ";
-      cin >> data.CodCliente;
+      cin >> voce0.CodCliente;
       cout << "Data: ";
-      cin >> data.Data;
+      cin >> voce0.Data;
       cout << "Importo: ";
-      cin >> data.Importo;
+      cin >> voce0.Importo;
       cout << "Pagato [SI/NO]: ";
-      cin >> data.Pagato;
-      write(data);
+      cin >> voce0.Pagato;
+      write();
       cout << "Record salvato con successo!" << endl;
 
     } else if (choice == 2) {
       cout << "Inserire il token da ricercare: ";
-      string token;
-      getchar();
-      getline(cin, token);
-      int n = find(token);
-      for (int i = 0; i <= n; i++){
-        cout << "\nRISULTATI:" << endl;
-        cout << "[" << i << "]\n    Codice fattura: " << r.CodFattura[i] << endl;
-        cout << "    Codice cliente: " << r.CodCliente[i] << endl;
-        cout << "    Data: " << r.Data[i] << endl;
-        cout << "    Importo: " << r.Importo[i] << endl;
-        cout << "    Pagato: " << r.Pagato[i] << endl << endl;
-      }
+      char token[100];
+      cin >> token;
+      fsearch(token);
+    }
   }
-
-}
-
   return 0;
 }
 
@@ -77,43 +56,48 @@ int main() {
 
 
 //@Override
-void write (struct voce data) {
-  fstream file;
-  file.open("Data.txt", ios::app);
-  if (file.is_open()) {
-    file << data.CodFattura << "-" << data.CodCliente << "-" << data.Data << "-" << data.Importo << "-" << data.Pagato << endl;
+void write () {
+  ofstream fout;
+	fout.open("Data.dat", ios::app | ios::binary);
+  if (!fout) {
+    cout << "Can't open Data.dat!" << endl;
+  } else {
+    fout.write((char *) &voce0, sizeof(voce0));
+    fout << "\r\n";
   }
-  file.close();
+  fout.close();
 }
 
 //@Override
-int find (string token) {
-  int counter = 0;
+void fsearch (char token[]) {
+  bool match;
+  ifstream fin;
+	fin.open("Data.dat", ios::in | ios::binary);
+  if (!fin) {
+    cout << "Can't open Data.dat!" << endl;
+  } else {
+    for (int i = 0; i < 10; i++) {
+      fin.read((char *) &voce0, sizeof(voce0));
 
-
-  fstream file;
-  file.open("Data.txt", ios::in);
-  if (file.is_open()) {
-    string ln[255];
-    for (int i = 0; i < 255; i++) { //ciclo delle linee (fatture)
-      getline(file, ln[i]);
-      if (ln[i].find(token) != string::npos) { //se viene rilevato il token
-        counter++;
-        char cln [ln[i].length()+1];
-        strcpy(cln, ln[i].c_str());
-        int data_index = 0;
-        for (int j = 0; j <= ln[i].length(); j++) { //ciclo dei caratteri
-          if (cln[j] == '-') data_index++;
-          if (cln[j] != '-' && data_index == 0) r.CodFattura[i] += cln[j];
-          if (cln[j] != '-' && data_index == 1) r.CodCliente[i] += cln[j];
-          if (cln[j] != '-' && data_index == 2) r.Data[i] += cln[j];
-          if (cln[j] != '-' && data_index == 3) r.Importo[i] += cln[j];
-          if (cln[j] != '-' && data_index == 4) r.Pagato[i] += cln[j];
-        }
-      }
+      if (voce0.CodFattura == token) out(true);
+      else if (voce0.CodCliente  == token) out(true);
+      else if (voce0.Data == token) out(true);
+      else if (voce0.Importo  == token) out(true);
+      else if (voce0.Pagato  == token) out(true);
+      else out(false);
     }
-
   }
-  file.close();
-  return counter;
+  fin.close();
+}
+
+//@Override
+void out(bool match) {
+  if (match) {
+    cout << "\nRISULTATI RICERCA:" << endl;
+    cout << "    Codice fattura: " << voce0.CodFattura << endl;
+    cout << "    Codice cliente: " << voce0.CodCliente << endl;
+    cout << "    Data: " << voce0.Data << endl;
+    cout << "    Importo: " << voce0.Importo << endl;
+    cout << "    Pagato: " << voce0.Pagato << endl << endl;
+  }
 }
