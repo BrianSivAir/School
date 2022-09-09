e49126ae86b249bcef5a7c54e6dd3ad0be825aee
Brr.dev <71598387+XTRIGGER00@users.noreply.github.com>
2022-09-09 13:07:33 +0200

    Added resolve function with address parameters

--- 3K/C_C++/Exercises_Equations_25-11-2021/2/equation2.cpp
+++ 3K/C_C++/Exercises_Equations_25-11-2021/2/equation2.cpp
@@ -4,42 +4,45 @@
 
 using namespace std;
 
-float a, b, c, delta, x1, x2;
 char repeat = 'y';
+
+void resolve(float a, float b, float c, float &x1, float &x2);
 
 int main() {
     cout << "2 degree equation\n" << endl;
     while (repeat == 'y' || repeat == 'Y') {
+        float a,b,c,x1,x2;
         do {
-            do {
-                cout << "Enter a: ";
-                cin >> a;
-                cout << "Enter b: ";
-                cin >> b;
-                cout << "Enter c: ";
-                cin >> c;
-                cout << "\n";
-            } while (a == 0);
+            cout << "Enter a: ";
+            cin >> a;
+            cout << "Enter b: ";
+            cin >> b;
+            cout << "Enter c: ";
+            cin >> c;
+            cout << "\n";
+        } while (a == 0);
 
-            delta = b * b -4 * a * c;
-            x1 = -(b / 2 * a);
-            x2 = x1;
-            if (delta < 0) cout << "\aEquation impossible in R" << endl;
-        } while (delta < 0);
+        resolve(a, b, c, x1, x2);
 
-        if (delta > 0) {
-            delta = sqrt(delta) / 2 * a;
-            x1 -= delta;
-            x2 += delta;
-        } else if (delta == 0) cout << "\aRadici reali e coincidenti" << endl;
         cout << "S: {" << x1 << ", " << x2 << "}" << endl;
         cout << "\nDo you want to repeat? [Y/N]";
         cin >> repeat;
         cout << "\n";
-        a = 0; b = 0; c = 0; delta = 0; x1 = 0; x2 = 0;
     }
+
+
     system("PAUSE");
 
     return 0;
 }
 
+void resolve(float a, float b, float c, float &x1, float &x2) {
+    float delta = b * b -4 * a * c;
+    x1 = -(b / 2 * a);
+    x2 = x1;
+    if (delta > 0) {
+        delta = sqrt(delta) / 2 * a;
+        x1 -= delta;
+        x2 += delta;
+    }
+}
