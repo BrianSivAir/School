0b9fa4d3c12a1e6ce3d5b3ec0fb95ed9b68fc785
Brr.dev <71598387+XTRIGGER00@users.noreply.github.com>
2022-09-12 11:23:19 +0200

    Added Exercises_Functions_12-09-2022

---
+++ 3K/C_C++/Exercises_Functions_12-09-2022/Main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#define MAX 20
+
+using namespace std;
+
+
+struct alunno{
+    char cognome[30];
+    char nome[20];
+    int voto;
+};
+
+bool confronto(string a, string b);
+
+int controllo_numAlunni();
+
+void input(alunno a[], int n);
+
+void ricerca(alunno a[], int n, string cercaCognome);
+
+
+int main() {
+
+    alunno a[MAX];
+
+    int num_alunni;
+
+    char cerca[20];
+
+    num_alunni = controllo_numAlunni();
+
+    input(a, num_alunni);
+
+    ricerca(a, num_alunni, cerca);
+
+    return 0;
+}
+
+bool confronto(string a, string b) {
+    if (a.length() != b.length()) {
+        return false;
+    }
+
+    int i = -1 ;
+    do {
+        i ++;
+        if (a[i] != b[i]) {
+            return false;
+        }
+    } while (i < a.length());
+
+    return true;
+}
+
+int controllo_numAlunni() {
+    int n;
+    do {
+        cout << "Inserisci il numero di alunni: ";
+        cin >> n ;
+    } while (n < 1 || n > MAX);
+
+    return n;
+}
+
+void input(alunno a[], int n) {
+    for (int i = 0 ; i < n ; i++) {
+        cout << "Inserisci il cognome dell'alunno: " ;
+        cin >> a[i].cognome;
+        cout << "Inserisci il nome dell'alunno: ";
+        cin >> a[i].nome;
+        cout << "Inserisci il voto: ";
+        cin >> a[i].voto;
+    }
+
+}
+
+void ricerca(alunno a[], int n, string cercaCognome) {
+    string cerca;
+    int trovato;
+    cout << "\nInserisci il cognome dell'alunno da cercare: ";
+    cin >> cerca;
+    trovato = 0 ;
+    for (int j = 0 ; j < n ; j ++ ) {
+        if (confronto(a[j].cognome, cerca)) {
+            cout << " \nAlunno trovato in posizione " << j+1 << " " << a[j].nome << " " << a[j].voto ;
+            trovato = 1 ;
+        }
+    }
+    if (!trovato) {
+        cout << "\nAlunno non trovato.\n ";
+    }
+}
